d74657c279bd079159bf298cd1b8c58d6366a994
Teo Lopez <teo.lopez@cabify.com>
2025-03-02 17:39:39 +0100

    build static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +6,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	adapter: adapter(),
+	adapter: {
+		pages: 'build',
+		assets: 'build',
+		fallback: undefined,
+		precompress: false,
+		strict: true
+	},
 	paths: {
 		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 	}
